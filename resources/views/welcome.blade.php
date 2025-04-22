bd323a027d1a59976e67f8249c3bd7f13587c72f
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-04-22 19:56:02 +0500

    home is proyekt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,11 @@
             </div>
         </main>
     @else
+
         <main class="flex-grow container mx-auto px-4 py-8">
             <h1 class="text-3xl font-bold mb-6">Followed Posts</h1>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                
                 @foreach ($posts as $post)
 
                     <div class="bg-white p-6 rounded-lg shadow-md">
