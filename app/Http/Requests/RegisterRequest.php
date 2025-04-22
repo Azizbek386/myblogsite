fd36cd528b292e89ce812a6f2bb25e03dc4335e7
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-04-22 19:44:37 +0500

    feat: validate register form inputs

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -22,6 +22,7 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
+            
             'name' => 'required|min:5',
             'username' => 'required|unique:users,username',
             'email' => 'required|email|unique:users,email',
