b497bbc47368badd19101c7cdc1d37f1fcae98c4
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-04-22 19:46:45 +0500

    feat: validate login form using form request

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -11,11 +11,12 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        
+
         return true;
     }
 
     /**
+     * 
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
